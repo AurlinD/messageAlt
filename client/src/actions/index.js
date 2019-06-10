dac5bb66b29b00e6731ae4fc6a4c834737c5ae43
Aurlin Dhillon <aurlindhillon@gmail.com>
2019-06-09 22:09:54 -0700

    programatic nav and url based selection

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from "../history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -35,6 +36,11 @@ export const createStream = formValues => async (dispatch, getState) => {
   const response = await streams.post("/streams", { ...formValues, userId });
   // axios return a bunch of different information
   dispatch({ type: CREATE_STREAM, payload: response.data });
+
+  // Do some programmatic navigation to
+  // get the user back to the root route
+  // push navigates the user
+  history.push("/");
 };
 
 export const fetchStreams = () => async dispatch => {
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+  // updates the redux store allowing us to see content
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    // content initially undefined
+    if (!this.props.stream) {
+      return <div>Loading..</div>;
+    } else {
+      return <div>{this.props.stream.title}</div>;
+    }
+  }
+}
+
+// if you need to access props from streamEdit using MSTP
+// use second props
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamEdit;
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamEdit);
