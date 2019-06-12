3f659e2afc1004fcdd0fcfbf71e82e3f0fd179b5
Aurlin Dhillon <aurlindhillon@gmail.com>
2019-06-12 14:55:51 -0700

    full implementation for MODAL with reusability

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,13 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 const Modal = props => {
   // specific function for modal
+  // creatportal used when you want to render something
+  // not created by your html or by your react applciation
+  // onClick triggers function when clicked upon
+  // e.stopPropgation stops the propagation up the tree
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">Are you sure?</div>
+    <div
+      onClick={() => history.push("/")}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={e => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
+      </div>
     </div>,
-    // call other div ID created and NOT root which it would default to
+    // call other div ID created speicfically for modal and NOT root which it would default to
     document.querySelector("#modal")
   );
 };
